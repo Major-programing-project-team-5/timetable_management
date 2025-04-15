aa252115f2fd74e608fa37e75615edd121abb5e7
ParkIsPark <157129209+ParkIsPark@users.noreply.github.com>
2025-04-15 14:34:11 +0900

    커밋
    .

---
+++ src/Core/PJG_testCLass_subject_algorithm.java
@@ -0,0 +1,8 @@
+package Core;
+
+public class PJG_testCLass_subject_algorithm {
+    public subject_temp findSubject(String input){
+        System.out.println("튜플을 받아서 분해 후 서브젝트로 출력하는 메서드");
+        return new subject_temp();//기본 서브젝트 제출. 
+    }
+}
