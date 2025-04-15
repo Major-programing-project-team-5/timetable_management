3cefb2b2e910cb55abc9940438864b06275d72bc
ParkIsPark <157129209+ParkIsPark@users.noreply.github.com>
2025-04-15 13:58:26 +0900

    파일 구조 정리 밑 main 폴더 만듬
    .

--- untitled/src/tempPjg.java
+++ untitled/src/tempPjg.java
@@ -9,4 +9,13 @@ public class tempPjg {
         pushSubjectFileTodatabase();
         pushTimetableFileTodatebase();
     }
+
+    private void pushSubjectFileTodatabase() {
+        System.out.println("서브젝트 파일에 변경사항 업로드 완료 - 구현필요");
+    }
+
+    private void pushTimetableFileTodatebase() {
+        System.out.println("타임테이블 파일에 변경사항 업로드 완료 - 구현필요");
+
+    }
 }
