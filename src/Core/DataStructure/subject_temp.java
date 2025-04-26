5cd61beafbf0a091a4550c7ceb06dc10808f4f61
eunseoinansan <fdd0907@gmail.com>
2025-04-26 15:27:54 +0900

    기초 객체 구현: subject_temp, timetable_temp, graduation_temp 추가

--- src/Core/DataStructure/subject_temp.java
+++ src/Core/DataStructure/subject_temp.java
@@ -1,8 +1,52 @@
 package Core.DataStructure;
 
-public class subject_temp {
-    //임시로 만들어둔 객체
-    public subject_temp(){
+public class Subject {
+    private String subjectName;
+    private int credit;
+    private String subjectCode;
+    private String category; // 예: 전공필수, 교양선택 등
 
+    public Subject(String subjectName, int credit, String subjectCode, String category) {
+        this.subjectName = subjectName;
+        this.credit = credit;
+        this.subjectCode = subjectCode;
+        this.category = category;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + subjectCode + "] " + subjectName + " (" + credit + "학점, " + category + ")";
     }
 }
