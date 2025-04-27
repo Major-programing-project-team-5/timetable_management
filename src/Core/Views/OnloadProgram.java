60f9d2c9c6d2c88e918a63307517d8c33dab5c0d
ParkIsPark <157129209+ParkIsPark@users.noreply.github.com>
2025-04-28 05:36:13 +0900

    Update OnloadProgram.java

--- src/Core/Views/OnloadProgram.java
+++ src/Core/Views/OnloadProgram.java
@@ -9,6 +9,8 @@ public class OnloadProgram {
 
     public void run(){
         update();
+        help_onStart();
+
     }
     public void update(){
         UpdateManager updateManager = new UpdateManager();
@@ -16,5 +18,30 @@ public class OnloadProgram {
         updateManager.updateTimetableManager("src/resources/timetable.txt");
         updateManager.updateGraduate("src/resources/graduate.txt");
     }
+    public void help_onStart(){
+
+        //'='는 한 줄에 30개
+        System.out.println("==============================");
+        System.out.println("시간표 관리 프로그램 시작");
+        System.out.println("==============================");
+        System.out.println("사용 가능한 명령어 목록입니다: ");
+        System.out.println();
+        System.out.println("help : 도움말 관련 기능을 제공합니다.");
+        System.out.println("예) help, help add");
+        System.out.println("quit : 프로그램을 종료합니다.");
+        System.out.println("add : 시간표 관련 데이터를 추가합니다.");
+        System.out.println("예) 시간표 생성, 과목 추가 등을 수행할 수 있습니다.");
+        System.out.println("verify : 지난 학기 시간표, 현재 시간표를 확인합니다.");
+        System.out.println("calc : 잔여 학점, 현재 학점, 과거 학점을 계산합니다.");
+        System.out.println("remove : 시간표 또는 과목 데이터를 삭제합니다.");
+        System.out.println("update : 과목 데이터베이스를 최신 상태로 불러옵니다.");
+        System.out.println();
+        System.out.println("명령어에 대한 자세한 설명은 'help [명령어]'를 입력해 확인하세요.");
+        System.out.println("예) help add");
+        System.out.println();
+        System.out.println("무엇을 도와드릴까요?");
+        System.out.print("> ");
+    }
+
 
 }
