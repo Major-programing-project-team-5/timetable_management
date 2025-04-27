f4274f968789e2cf98d45ba25eee438dc7145104
BellBirdP80 <danmark317@gmail.com>
2025-04-28 01:09:48 +0900

    Update PJG_testClass_quit - 김도연
    김도연 님 작업분을 대신 커밋했습니다

--- src/Core/Utils/Quit/PJG_testClass_quit.java
+++ src/Core/Utils/Quit/PJG_testClass_quit.java
@@ -1,25 +1,53 @@
 package Core.Utils.Quit;
 
-//to-do
+import Core.DataStructure.Subject;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class PJG_testClass_quit {
 
-    public void quit(){
-        uploadAllFileTodataBase();
+    private List<Subject> subjectBuffer;
+    private Timetable timetableBuffer;
+
+    public PJG_testClass_quit(List<Subject> subjectBuffer, Timetable timetableBuffer) {
+        this.subjectBuffer = subjectBuffer;
+        this.timetableBuffer = timetableBuffer;
     }
 
-    private void uploadAllFileTodataBase() {
-        pushSubjectFileTodatabase();
-        pushTimetableFileTodatebase();
-
-        System.out.println("프로그램을 종료합니다");
+    public void quit() {
+        uploadAllFileToDatabase();
+        System.out.println("프로그램을 종료합니다.");
+        System.exit(0);  // 프로그램 완전 종료
     }
 
-    private void pushSubjectFileTodatabase() {
-        System.out.println("서브젝트 파일에 변경사항 업로드 완료 - 구현필요");
+    private void uploadAllFileToDatabase() {
+        pushSubjectFileToDatabase();
+        pushTimetableFileToDatabase();
+        // 나중에 graduate.txt 저장 추가 가능
     }
 
-    private void pushTimetableFileTodatebase() {
-        System.out.println("타임테이블 파일에 변경사항 업로드 완료 - 구현필요");
+    private void pushSubjectFileToDatabase() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("subject.txt"))) {
+            for (Subject subject : subjectBuffer) {
+                writer.write(subject.toString());
+                writer.newLine();
+            }
+            System.out.println("subject.txt에 저장 완료");
+        } catch (IOException e) {
+            System.out.println("subject.txt 저장 실패: " + e.getMessage());
+        }
+    }
 
+    private void pushTimetableFileToDatabase() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("timetable.txt"))) {
+            for (Subject subject : timetableBuffer.getSubjects()) {
+                writer.write(subject.toString());
+                writer.newLine();
+            }
+            System.out.println("timetable.txt에 저장 완료");
+        } catch (IOException e) {
+            System.out.println("timetable.txt 저장 실패: " + e.getMessage());
+        }
     }
 }
